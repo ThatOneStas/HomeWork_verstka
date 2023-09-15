3861208516f2364f7a6c974e7415f6ae0e9eb163
ThatOneStas <138216528+ThatOneStas@users.noreply.github.com>
2023-09-15 15:29:36 +0300

    15.09

--- ITmisto/index.js
+++ ITmisto/index.js
@@ -1,6 +1,8 @@
 const BURGER_BTN = document.querySelector(".burger")
+const HEADER = document.querySelector("header")
 BURGER_BTN.addEventListener("click", ()=>{
     BURGER_BTN.classList.toggle('active')
+    HEADER.classList.toggle('active')
     const MOB_MENU = document.querySelector(".menu")
     MOB_MENU.classList.toggle('active')
 })
